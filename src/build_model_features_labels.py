603213551112e23033d548d1f8cd0dcd448f5aaa
Chris Schultz <chris@Chriss-MacBook-Pro.local>
2017-02-02 14:04:22 -0600

    built model labels

--- src/build_model_features_labels.py
+++ src/build_model_features_labels.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 
-def build_labels:
+def build_labels():
     '''
     Builds default loan model's classification labels, i.e. dependent variable
 
@@ -10,11 +10,16 @@ def build_labels:
 
     Ignoring Current because outcome of the loan is not known yet and this model is focused
     on the final outcome of loan if it was given to an applicant, i.e. going to default (positive label) or fully pay (negative label)
+
+    Saves results in:
+        data/model_labels.csv
     '''
-    raw_data_df = pd.read_csv('data/data.csv')
+    loan_data_df = pd.read_csv('data/data.csv').set_index("id")
+    loan_data_df = loan_data_df[loan_data_df['loan_status'] != 'Current']
+    loan_data_df['loan_status'].replace({"Fully Paid":0, "Default":1}, inplace=True)
+    loan_data_df.ix[:, ['loan_status']].to_csv('data/model_labels.csv')
 
-
-def build_features:
+def build_features():
     '''
     '''
     pass
