9fce5d19aedef590150851009bd8610848f32421
Chris Schultz <chris@Chriss-MacBook-Pro.local>
2017-02-02 14:29:16 -0600

    started feature building code

--- src/build_model_features_labels.py
+++ src/build_model_features_labels.py
@@ -21,10 +21,18 @@ def build_labels():
 
 def build_features():
     '''
+    Saves results in:
+        data/model_features.csv
     '''
-    pass
+    loan_data_df = pd.read_csv('data/data.csv').set_index("id") # load in data
+    loan_data_df = loan_data_df[loan_data_df['loan_status'] != 'Current'] # remove current status entries
+    loan_data_df = loan_data_df.drop('loan_status', 1)  # remove label
+
+    # grab columns from dataframe that will be turned into features for this model
+    columns_to_use_as_features = ['loan_amnt', 'term', 'installment', 'emp_length']
 
 
 
 if __name__ == '__main__':
     build_labels()
+    build_features()
